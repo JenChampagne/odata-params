6220fca0cc920cacf01aaa7c93c641d9daec2fab
Jenifer Champagne <jen@iferc.ca>
2024-06-24 22:25:30 -0400

    Add support for comparison operators: eq ne lt le gt ge.

--- src/filters.rs
+++ src/filters.rs
@@ -1,4 +1,5 @@
 use bigdecimal::BigDecimal;
+use std::str::FromStr;
 
 pub use odata_filter::parse_str;
 
@@ -6,10 +7,21 @@ pub use odata_filter::parse_str;
 pub enum Expr {
     Or(Box<Expr>, Box<Expr>),
     And(Box<Expr>, Box<Expr>),
+    Compare(Box<Expr>, CompareOperator, Box<Expr>),
     Not(Box<Expr>),
     Function(String, Vec<Expr>),
     Identifier(String),
     Value(Value),
+}
+
+#[derive(Debug)]
+pub enum CompareOperator {
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterOrEqual,
+    LessThan,
+    LessOrEqual,
 }
 
 #[derive(Debug)]
@@ -22,7 +34,7 @@ pub enum Value {
 
 peg::parser! {
     grammar odata_filter() for str {
-        use super::{Expr, Value};
+        use super::{Expr, CompareOperator, Value};
 
         pub rule parse_str() -> Expr
             = filter()
@@ -34,13 +46,25 @@ peg::parser! {
             / any_expr()
 
         rule any_expr() -> Expr
-            = value_expr()
+            = comparison_expr()
+            / value_expr()
             / "(" _ e:filter() _ ")" { e }
 
         rule value_expr() -> Expr
             = function_call()
             / v:value() { Expr::Value(v) }
             / i:identifier() { Expr::Identifier(i) }
+
+        rule comparison_expr() -> Expr
+            = l:value_expr() _ op:comparison_op() _ r:value_expr() { Expr::Compare(Box::new(l), op, Box::new(r)) }
+
+        rule comparison_op() -> CompareOperator
+            = "eq" { CompareOperator::Equal }
+            / "ne" { CompareOperator::NotEqual }
+            / "gt" { CompareOperator::GreaterThan }
+            / "ge" { CompareOperator::GreaterOrEqual }
+            / "lt" { CompareOperator::LessThan }
+            / "le" { CompareOperator::LessOrEqual }
 
         rule function_call() -> Expr
             = fname:identifier() _ "(" _ args:filter_list() _ ")" { Expr::Function(fname, args) }
